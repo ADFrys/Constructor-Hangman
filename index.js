f72a28c53121259ff457c054fb7531fc74e9dfea
AngelaF <ADFrys@users.noreply.github.com>
2018-03-19 15:47:54 -0500

    Update function for incorrect letter guessed. Also cleanup code.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var Word = require("./Word.js");
 var inquirer = require("inquirer");
 
 
-
 // Word bank of words user will guess
 var words = [
   "bird",
@@ -16,47 +15,9 @@ var words = [
 
 var user = new Word(words);
 
-
-
-
-
-
-
-// Function that selects a random word from the word bank and creates underscores (_) for each letter.
-// This might need to be moved into the Word constructor... this may not be the correct setup.
-// Not sure how this should interact with the Letter Constructor...
-// var randomWord = function () {
-//   word = words[Math.floor(Math.random() * words.length)];
-
-//   // var remainingLetters = word.length; 
-//   var answerArray = []; // dashed
-
-//   // loops through word assigned to create appropriate number of underscores (_)
-//   for (var i = 0; i < word.length; i++) {
-//     answerArray[i] = "_";
-//   };
-//   // Shows the hidden word at the start of the game
-//   console.log("Here is the word for you to guess " + answerArray);
-
-//   return answerArray; // ["_", "_", "_", "_"]
-// };
-// call the random word function described above
 var dashed = user.randomWord();
 
-console.log("Word:  " + user.word);
-  
-
-// reset function that calls a random word to be selected from the word bank and displays the new word to guess.
-// var reset = function () {
-//   randomWord();
-//   console.log("next word to guess " + word);
-// }
-
-// Inquirer prompt asking for a letter to guess.
-// Inquirer prompts should repeat until the word is guessed. 
-// if word.length has 1 or more placeholders _ , repeat inquirer prompts... If won, restart inquirer prompts
-
-// Recursion
+// console.log("Word:  " + user.word);
 
 
 function askQuestion() {
@@ -71,7 +32,7 @@ function askQuestion() {
     .then(function (answers) {
       // stores inquirer guess into a variable
       // console.log(answers);
-      // var character = answers.character;
+      var character = answers.character;
       // Adds to the word constructor. Stores character (guess).
       // var newGuess = new Word(character);
 
@@ -93,9 +54,7 @@ function askQuestion() {
         
 
       } else {
-        // TODO
-        // It doesn't not exist
-        console.log("No it does not");
+        console.log("Sorry, that is not the correct letter.");
         askQuestion();
 
       }
@@ -105,10 +64,3 @@ function askQuestion() {
 }
 
 askQuestion();
-  // Not sure how the letter and word constructors interact to create the game logic
-
- // call the reset function after word is guessed correctly
-
-
- // Feature
- // 
